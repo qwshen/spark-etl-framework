97d6c22f097c247b48da809578ed1dc50b912b0a
qwshen <willy_shen@hotmail.com>
2022-09-24 12:17:21 -0400

    fixed one type-match issue

--- src/main/scala/com/qwshen/common/ValueOperator.scala
+++ src/main/scala/com/qwshen/common/ValueOperator.scala
@@ -27,24 +27,31 @@ trait ValueOperator extends Serializable {
    * @return - the new value (from value) which matches the type of the member
    */
   def matchValue(member: TermSymbol, value: AnyRef): AnyRef = member.typeSignature.resultType.toString match {
-    case ValueOperator.optionType(tpe) => tpe match {
-      case "Int" => Option[AnyRef](Int.box(Try(value.toString.toInt).getOrElse(value.toString.toFloat.toInt)))
-      case "Boolean" => Option[AnyRef](Boolean.box(value.toString.toBoolean))
-      case "Long" => Option[AnyRef](Long.box(Try(value.toString.toLong).getOrElse(value.toString.toDouble.toLong)))
-      case "Byte" => Option[AnyRef](Byte.box(Try(value.toString.toByte).getOrElse(value.toString.toFloat.toByte)))
-      case "Short" => Option[AnyRef](Short.box(Try(value.toString.toShort).getOrElse(value.toString.toFloat.toShort)))
-      case "Float" => Option[AnyRef](Float.box(value.toString.toFloat))
-      case "Double" => Option[AnyRef](Double.box(value.toString.toDouble))
+    case ValueOperator.optionType(_, t) => t match {
+      case ValueOperator.regularType(_, st) => st match {
+        case "String" | "Char" => Option[AnyRef](value.toString)
+        case "Int" => Option[AnyRef](Int.box(Try (value.toString.toInt).getOrElse(value.toString.toFloat.toInt)))
+        case "Boolean" => Option[AnyRef](Boolean.box(value.toString.toBoolean))
+        case "Long" => Option[AnyRef](Long.box(Try(value.toString.toLong).getOrElse(value.toString.toDouble.toLong)))
+        case "Byte" => Option[AnyRef](Byte.box(Try(value.toString.toByte).getOrElse(value.toString.toFloat.toByte)))
+        case "Short" => Option[AnyRef](Short.box(Try(value.toString.toShort).getOrElse(value.toString.toFloat.toShort)))
+        case "Float" => Option[AnyRef](Float.box(value.toString.toFloat))
+        case "Double" => Option[AnyRef](Double.box (value.toString.toDouble))
+        case _ => Option[AnyRef](value)
+      }
       case _ => Option[AnyRef](value)
     }
-    case "Int" => Int.box(Try(value.toString.toInt).getOrElse(value.toString.toFloat.toInt))
-    case "Long" => Long.box(Try(value.toString.toLong).getOrElse(value.toString.toDouble.toLong))
-    case "Boolean" => Boolean.box(value.toString.toBoolean)
-    case "Byte" => Byte.box(Try(value.toString.toByte).getOrElse(value.toString.toFloat.toByte))
-    case "Short" => Short.box(Try(value.toString.toShort).getOrElse(value.toString.toFloat.toShort))
-    case "Float" => Float.box(value.toString.toFloat)
-    case "Double" => Double.box(value.toString.toDouble)
-    case "Char" | "String" => value.toString
+    case ValueOperator.regularType(_, t) => t match {
+      case "String" | "Char" => value.toString
+      case "Int" => Int.box(Try(value.toString.toInt).getOrElse(value.toString.toFloat.toInt))
+      case "Long" => Long.box(Try(value.toString.toLong).getOrElse(value.toString.toDouble.toLong))
+      case "Boolean" => Boolean.box(value.toString.toBoolean)
+      case "Byte" => Byte.box(Try(value.toString.toByte).getOrElse(value.toString.toFloat.toByte))
+      case "Short" => Short.box(Try(value.toString.toShort).getOrElse(value.toString.toFloat.toShort))
+      case "Float" => Float.box(value.toString.toFloat)
+      case "Double" => Double.box(value.toString.toDouble)
+      case _ => value
+    }
     case _ => value
   }
 
@@ -62,6 +69,8 @@ trait ValueOperator extends Serializable {
 }
 
 object ValueOperator {
+  //the regular-expression for identifying Regular Types
+  private val regularType = "(java.lang.|scala.)?(.+)".r
   //the regular-expression for identifying Option Type
-  private val optionType = "Option\\[(.+)\\]".r
+  private val optionType = "(scala.)?Option\\[(.+)\\]".r
 }
